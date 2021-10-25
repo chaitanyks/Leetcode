b7b26652884b41eae85154066fb944f572b8a61c
Undefined <defcdr@gmail.com>
2021-10-25 05:50:35 +0530

    Min Stack

---
+++ Min Stack.cpp
@@ -0,0 +1,37 @@
+
+class MinStack {
+public:
+    multiset<int> s;
+    stack<int> st;
+
+    MinStack() {
+
+    }
+
+    void push(int val) {
+        s.insert(val);
+        st.push(val);
+    }
+
+    void pop() {
+        s.erase(s.find(st.top()));
+        st.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return *s.begin();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
