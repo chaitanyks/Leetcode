9a84ed1f38b44d6369e562b4af73eefc3544d30a
Undefined <defcdr@gmail.com>
2022-01-25 09:16:32 +0530

    941. Valid Mountain Array

---
+++ Valid Mountain Array.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-mountain-array/
+// 941. Valid Mountain Array
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>&arr) {
+        int n = arr.size();
+        int i = 0;
+        
+        while (i < n - 1 && arr[i] < arr[i + 1])
+            i++;
+
+        if (i == 0 || i == n - 1)
+            return false;
+
+        while (i < n - 1 && arr[i] > arr[i + 1])
+            i++;
+
+        if (i == n - 1)
+            return true;
+        else
+            return false;
+    }
+};
