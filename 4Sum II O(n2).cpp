c45650517a079d34d8ecdce37fa2e981e73cb076
Undefined <defcdr@gmail.com>
2022-02-03 10:39:36 +0530

    454. 4Sum II
    https://leetcode.com/problems/4sum-ii/
    454. 4Sum II

---
+++ 4Sum II O(n2).cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/4sum-ii/
+// 454. 4Sum II
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int n = nums1.size();
+        
+        int ans = 0;        
+        map<int,int> mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int sum = nums3[i]+nums4[j];
+                mp[sum]++;
+            }
+        }     
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int sum = nums1[i] + nums2[j];
+                sum = -sum;
+                ans = ans + mp[sum];
+            }
+        }
+        
+        return ans;
+    }
+};
